7d1b9a21db679ed76eff893630c59e56561bf6ae
PatrickOjiambo <pashrick237@students.uonbi.ac.ke>
2023-05-04 13:03:24 -0700

    new

--- 0x14-bit_manipulation/1-print_binary.c
+++ 0x14-bit_manipulation/1-print_binary.c
@@ -6,23 +6,9 @@
  */
 void print_binary(unsigned long int n)
 {
-	unsigned int flag = 0, max = 32768; /* 1000 0000 0000 0000 */
-
-	if (n == 0)
+      	if (n > 1)
 	{
-		_putchar('0');
-		return;
+	    	print_binary(n >> 1);
 	}
-	while (max)
-	{
-		if (flag == 1 && (n & max) == 0)
-			_putchar('0');
-		else if ((n & max) != 0)
-		{
-			_putchar('1');
-			flag = 1;
-		}
-		max >>= 1;
-	}
+      	putchar((n & 1) ? '1' : '0');
 }
-
